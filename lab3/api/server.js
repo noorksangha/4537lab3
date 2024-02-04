9a05a202a83a3629721325893cba85be625f6c3e
noorksangha <113377440+noorksangha@users.noreply.github.com>
2024-02-04 15:38:56 -0800

    Update server.js

--- lab3/api/server.js
+++ lab3/api/server.js
@@ -1,12 +1,14 @@
-const utils = require('./modules/util.js');
+const utils = require('./modules/util.js'); // Make sure this path is correct
 const http = require('http');
 const url = require('url');
 const message = require("./lang/en/en.js")
 
-
 http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   let name = req.url.split("=")[1];
-  res.write(`<p style="color: blue;">${message.greeting.replace("%1",name)} ${date.getDate().toString()}</p>`);
+  // Use the `getDate` method from `utils` module
+  let currentDate = utils.getDate();
+  // Now use `currentDate` to get the date
+  res.write(`<p style="color: blue;">${message.greeting.replace("%1",name)} ${currentDate.getDate().toString()}</p>`);
   res.end();
 }).listen(8080);
